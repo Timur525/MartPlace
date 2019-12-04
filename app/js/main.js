06a6829f664d5552128c6c21ce9ac33609804b52
Timur525 <7152521@gmail.com>
2019-12-04 18:12:03 +1000

    added section slider and section mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
 $(function(){
 
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "13px",
+    readOnly: true,
+  });
   
+  $(".slider__wrapper-product").slick({
+    prevArrow: '<button class="slick-arrow slick-prev lnr-chevron-right"></button>',
+    nextArrow: '<button class="slick-arrow slick-next lnr-chevron-left"></button>',
+  });
+
+  var mixer = mixitup('.products__inner-box')
+
 });
